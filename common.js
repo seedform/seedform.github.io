7fb1b8352bc5c31c12e85e86ae405d49d482cb39
Shudmanul Chowdhury <seedformapps@gmail.com>
2014-01-02 14:02:53 -0500

    Added fancy animations

--- common.js
+++ common.js
@@ -14,13 +14,34 @@ function nav() {
 function footer() {
     document.write(
         "<footer>" +
-        "<div style=\"margin-bottom: 6px;\">" +
-            "<a href=\"https://github.com/seedform\" class=\"text-link\">" +
-                "<img src=\"img/GitHub-Mark-32px.png\" style=\"vertical-align: middle;\"/>" +
-                "<span style=\"vertical-align: middle;\">&nbsp;&nbsp;&nbsp;Visit my GitHub profile</span>" +
-            "</a>" +
-        "</div>" +
-        "&#169; <script>document.write(new Date().getFullYear());</script> Shudmanul Chowdhury" +
+            "<div style=\"margin-bottom: 6px;\">" +
+                "<a href=\"https://github.com/seedform\" class=\"text-link\">" +
+                    "<img src=\"img/GitHub-Mark-32px.png\" style=\"vertical-align: middle;\"/>" +
+                    "<span style=\"vertical-align: middle;\">&nbsp;&nbsp;&nbsp;Visit my GitHub profile</span>" +
+                "</a>" +
+            "</div>" +
+            "&#169; " + new Date().getFullYear() + " Shudmanul Chowdhury" +
         "</footer>"
     );
 }
+
+
+//Taken from http://jsfiddle.net/vincentieo/6K9SZ/
+// The speed of one transition (fadeIn or fadeOut).
+// Full execution time will be:
+// (browser navigation time) + speed * 2;
+// You can set this to slow, medium, fast, or number of ms.
+var speed = 'medium';
+$('html, body').hide();
+$(document).ready(function() {
+    $('html, body').fadeIn(speed, function() {
+        $('a[href], button[href]').click(function(event) {
+            var url = $(this).attr('href');
+            if (url.indexOf('#') == 0 || url.indexOf('javascript:') == 0) return;
+            event.preventDefault();
+            $('html, body').fadeOut(speed, function() {
+                window.location = url;
+            });
+        });
+    });
+});

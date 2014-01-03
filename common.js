99ac76a58212794940e062b3bca1d47723b507b1
Shudmanul Chowdhury <seedformapps@gmail.com>
2014-01-03 00:26:27 -0500

    Removed redundant css code, centralized js code

--- common.js
+++ common.js
@@ -2,39 +2,42 @@ function nav() {
     var page = location.pathname.substring(location.pathname.lastIndexOf("/") + 1, location.pathname.lastIndexOf("."));
     document.write(
         "<div id=\"nav-bar\" class=\"ac\">" +
-                "<a class=\"nav-item " + (page == "/" ? "active" : "") + "\" href=\"http://seedform.github.io/\">PROJECTS</a>" + " | " +
-                "<a class=\"nav-item " + (page == "about" ? "active" : "") + "\" href=\"about.html\">ABOUT</a>" + " | " + 
-                "<a class=\"nav-item " + (page == "contact" ? "active" : "") + "\" href=\"contact.html\">CONTACT</a>" +
-        "</div>"
+                '<a class="nav-item ' + (page == '/' ? 'active' : '') + '" href="http://seedform.github.io/">PROJECTS</a>' + ' | ' +
+                '<a class="nav-item ' + (page == 'about' ? 'active' : '') + '" href="about.html">ABOUT</a>' + ' | ' + 
+                '<a class="nav-item ' + (page == 'contact' ? 'active' : '') + '" href="contact.html">CONTACT</a>' +
+        '</div>'
     );
 }
 
 function footer() {
     document.write(
-        "<footer>" +
-            "<hr>" +
-            "<div style=\"margin-bottom: 6px;\">" +
-                "<a href=\"https://github.com/seedform\" class=\"text-link\">" +
-                    "<img src=\"img/GitHub-Mark-32px.png\" style=\"vertical-align: middle;\"/>" +
-                    "<span style=\"vertical-align: middle;\">&nbsp;&nbsp;&nbsp;Visit my GitHub profile</span>" +
-                "</a>" +
-            "</div>" +
-            "<hr>" +
-            "&#169; " + new Date().getFullYear() + " Shudmanul Chowdhury" +
+        '<footer>' +
+            '<hr>' +
+            '<div style="margin-bottom: 6px;">' +
+                '<a href="https://github.com/seedform" class="text-link">' +
+                    '<img src="img/GitHub-Mark-32px.png" style="vertical-align: middle;"/>' +
+                    '<span style="vertical-align: middle;">&nbsp;&nbsp;&nbsp;Visit my GitHub profile</span>' +
+                '</a>' +
+            '</div>' +
+            '<hr>' +
+            '&#169; ' + new Date().getFullYear() + ' Shudmanul Chowdhury' +
         "</footer>"
     );
 }
 
-//Taken from http://jsfiddle.net/vincentieo/6K9SZ/
-// The speed of one transition (fadeIn or fadeOut).
-// Full execution time will be:
-// (browser navigation time) + speed * 2;
-// You can set this to slow, medium, fast, or number of ms.
+document.write(
+    '<link rel="stylesheet" href="stylesheet.css" type="text/css" charset="utf-8"/>' +
+    '<link href="http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,300italic,700" rel="stylesheet" type="text/css">' +
+    '<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />' +
+    '<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>'
+);
+
+// Fading transition, taken from http://jsfiddle.net/vincentieo/6K9SZ/
 var speed = 'medium';
 $('html, body').hide();
 $(document).ready(function() {
     $('html, body').fadeIn(speed, function() {
-        $('a[href], button[href]').click(function(event) {
+        $('a.nav-item').click(function(event) {
             var url = $(this).attr('href');
             if (url.indexOf('#') == 0 || url.indexOf('javascript:') == 0) return;
             event.preventDefault();
@@ -42,5 +45,12 @@ $(document).ready(function() {
                 window.location = url;
             });
         });
+    });
+});
+
+// Open all links in a new tab
+$(document).ready(function() { 
+    $('a.page-item, a.text-link').click(function(event) {
+        $('a').attr('target','_blank');
     });
 });

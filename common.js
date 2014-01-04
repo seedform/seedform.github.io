647500322efdbc3540091d843be6ddf4827ce713
Shudmanul Chowdhury <seedformapps@gmail.com>
2014-01-03 22:28:08 -0500

    JQuery now generating nav and footer

--- common.js
+++ common.js
@@ -1,37 +1,3 @@
-function nav() {
-    var page = location.pathname.substring(location.pathname.lastIndexOf("/") + 1, location.pathname.lastIndexOf("."));
-    document.write(
-        "<div id=\"nav-bar\" class=\"ac\">" +
-                '<a class="nav-item ' + (page == '/' ? 'active' : '') + '" href="http://seedform.github.io/">PROJECTS</a>' + ' | ' +
-                '<a class="nav-item ' + (page == 'about' ? 'active' : '') + '" href="about.html">ABOUT</a>' + ' | ' + 
-                '<a class="nav-item ' + (page == 'contact' ? 'active' : '') + '" href="contact.html">CONTACT</a>' +
-        '</div>'
-    );
-}
-
-function footer() {
-    document.write(
-        '<footer>' +
-            '<hr>' +
-            '<div style="margin-bottom: 6px; font-size:120%;">' +
-                '<a href="https://github.com/seedform" title="Visit my GitHub profile" class="text-link">' +
-                    '<img src="img/GitHub-Mark-120px-plus.png" style="width:32px; height:32px; vertical-align:middle;"/>' +
-                '</a>' +
-                '&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;' +
-                '<a href="https://twitter.com/seedform" title="Follow me on Twitter" class="text-link">' +
-                    '<img src="img/Twitter_logo_blue.png" style="width:32px; height:32px; vertical-align:middle;"/>' +
-                '</a>' +
-                '&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;' +
-                '<a href="https://plus.google.com/u/0/108831680097190194078/posts" title="Add me on Google+" class="text-link">' +
-                    '<img src="img/Red-signin-Small-base-44dp.png" style="width:36px; height:36px; vertical-align:middle;"/>' +
-                '</a>' +
-            '</div>' +
-            '<hr>' +
-            '&#169; ' + new Date().getFullYear() + ' Shudmanul Chowdhury' +
-        "</footer>"
-    );
-}
-
 document.write(
     '<link rel="shortcut icon" href="img/icon.png">' +
     '<link rel="stylesheet" href="stylesheet.css" type="text/css" charset="utf-8"/>' +
@@ -39,10 +5,39 @@ document.write(
     '<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />'
 );
 
-// Fading transition, taken from http://jsfiddle.net/vincentieo/6K9SZ/
-var speed = 'medium';
-$('html, body').hide();
 $(document).ready(function() {
+    
+    // Writes the nav-bar to the page
+    var page = location.pathname.substring(location.pathname.lastIndexOf("/") + 1, location.pathname.lastIndexOf("."));
+    $("#nav").html(
+        '<a class="nav-item ' + (page == '/' ? 'active' : '') + '" href="http://seedform.github.io/">PROJECTS</a>' + ' | ' +
+        '<a class="nav-item ' + (page == 'about' ? 'active' : '') + '" href="about.html">ABOUT</a>' + ' | ' + 
+        '<a class="nav-item ' + (page == 'contact' ? 'active' : '') + '" href="contact.html">CONTACT</a>'
+    );
+
+    // Writes the footer to the page
+    $("#foot").html(
+        '<hr>' +
+        '<div style="margin-bottom: 6px; font-size:120%;">' +
+            '<a href="https://github.com/seedform" title="Visit my GitHub profile" class="text-link">' +
+                '<img src="img/GitHub-Mark-120px-plus.png" style="width:32px; height:32px; vertical-align:middle;"/>' +
+            '</a>' +
+            '&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;' +
+            '<a href="https://twitter.com/seedform" title="Follow me on Twitter" class="text-link">' +
+                '<img src="img/Twitter_logo_blue.png" style="width:32px; height:32px; vertical-align:middle;"/>' +
+            '</a>' +
+            '&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;' +
+            '<a href="https://plus.google.com/u/0/108831680097190194078/posts" title="Add me on Google+" class="text-link">' +
+                '<img src="img/Red-signin-Small-base-44dp.png" style="width:36px; height:36px; vertical-align:middle;"/>' +
+            '</a>' +
+        '</div>' +
+        '<hr>' +
+        '&#169; ' + new Date().getFullYear() + ' Shudmanul Chowdhury'
+    );
+    
+    // Fading transition, taken from http://jsfiddle.net/vincentieo/6K9SZ/
+    var speed = 'medium';
+    $('html, body').hide();
     $('html, body').fadeIn(speed, function() {
         $('a.nav-item').click(function(event) {
             var url = $(this).attr('href');
@@ -53,11 +48,10 @@ $(document).ready(function() {
             });
         });
     });
-});
-
-// Open all links in a new tab
-$(document).ready(function() { 
-    $('a.page-item, a.text-link').click(function(event) {
+    
+    // Sets all non-navigation links to open in a new tab
+    $('a:not(".nav-item")').click(function(event) {
         $('a').attr('target','_blank');
     });
+    
 });

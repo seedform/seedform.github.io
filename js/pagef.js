6ccfdb9c91a56fdf918cfd46dc69c31e011d2fab
Shudmanul Chowdhury <seedformapps@gmail.com>
2014-01-06 21:31:44 -0500

    Cleaned up some js

--- js/pagef.js
+++ js/pagef.js
@@ -1,41 +1,54 @@
 var page = location.pathname.substring(location.pathname.lastIndexOf("/") + 1);
-alert(page);
-var prev = document.referrer == '' ? '/' : document.referrer.substring(document.referrer.lastIndexOf('/') + 1, document.referrer.lastIndexOf('.'));
+var prev = document.referrer.substring(document.referrer.lastIndexOf('/') + 1);
 
-var index   = 'http://www.shudman.com/';
-var about   = 'http://www.shudman.com/about.html';
-var contact = 'http://www.shudman.com/contact.html';
+var navHrefs = new Array(
+    '/',
+    'about.html',
+    'contact.html'
+);
+
+var navTitles = new Array(
+    'PROJECTS',
+    'ABOUT',
+    'CONTACT'
+);
+
+var socHrefs = new Array(
+    'https://github.com/seedform',
+    'https://twitter.com/seedform',
+    'https://plus.google.com/u/0/108831680097190194078/posts'
+);
+
+var socImgs = new Array(
+    'GitHub-Mark-120px-plus.png',
+    'Twitter_logo_blue.png',
+    'Red-signin-Small-base-44dp.png'
+);
+
+var socTitles = new Array(
+    'Visit my GitHub profile',
+    'Follow me on Twitter',
+    'Adde me on Google+'
+);
+
 
 function navFoot() {
 
     // Write the nav-bar to the page
-    $("#nav").html(
-       '<div class="vac"> \
-            <a class="nav-item ' + (page == '/' ? 'active' : '') + '" href="http://www.shudman.com/">PROJECTS</a>&nbsp;|&nbsp; \
-            <a class="nav-item ' + (page == 'about' ? 'active' : '') + '" href="about.html">ABOUT</a>&nbsp;|&nbsp; \
-            <a class="nav-item ' + (page == 'contact' ? 'active' : '') + '" href="contact.html">CONTACT</a> \
-        </div>'
-    );
+    var nav = '<div class="vac">';
+    for (var i = 0; i < navHrefs.length; i++) {
+        nav += '<a class="nav-item ' + (page == navHrefs[i] ? 'active' : '') + '" href="' + navHrefs[i] + '">' + navTitles[i] + '</a>';
+        nav += i < navHrefs.length - 1 ? '&nbsp;|&nbsp;' : '';
+    }
+    $("#nav").html(nav + '</div>');
 
     // Write the footer to the page
-    $("#foot").html(
-       '<hr> \
-        <div style="margin-bottom: 6px; font-size:120%;"> \
-            <a href="https://github.com/seedform" title="Visit my GitHub profile" class="text-link"> \
-                <img src="img/GitHub-Mark-120px-plus.png" style="width:32px; height:32px; vertical-align:middle;"/> \
-            </a> \
-            &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp; \
-            <a href="https://twitter.com/seedform" title="Follow me on Twitter" class="text-link"> \
-                <img src="img/Twitter_logo_blue.png" style="width:32px; height:32px; vertical-align:middle;"/> \
-            </a> \
-            &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp; \
-            <a href="https://plus.google.com/u/0/108831680097190194078/posts" title="Add me on Google+" class="text-link"> \
-                <img src="img/Red-signin-Small-base-44dp.png" style="width:36px; height:36px; vertical-align:middle;"/> \
-            </a> \
-        </div> \
-        <hr> \
-        Copyright &#169; ' + new Date().getFullYear() + ' Shudmanul Chowdhury'
-    );
+    var foot = '<hr><div style="margin-bottom: 6px; font-size:120%;">';
+    for (var i = 0; i < socHrefs.length; i++) {
+        foot += '<a href="' + socHrefs[i] + '" title="' + socTitles[i] + '" class="text-link"> <img src="img/' + socImgs[i] + '" class="footer-img-lnk"/> </a>';
+        foot += i < socHrefs.length - 1 ? '&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;' : '';
+    }
+    $("#foot").html(foot + '</div> <hr> Copyright &#169; ' + new Date().getFullYear() + ' Shudmanul Chowdhury');
 }
 
 // Set the footer position to be static or fixed based on screen size
@@ -45,24 +58,27 @@ function fixFoot() {
         $('#page-content').css('padding-bottom', '20px');
     } else {
         $('#foot').css('position', 'fixed');
-        $('#page-content').css('padding-bottom', '130px');
+        $('#page-content').css('padding-bottom', '110px');
     }
 }
 
 // Set transition animations
 function setAnimation() {
     var animIn, animOut;
-    
-    if (prev == '' || prev == '/' || prev == 'about' && page == 'contact') animIn='left-fade-in';
-    else if (prev == 'contact' || prev == 'about' && page == '/') animIn='right-fade-in';
+    if (page == navHrefs[0] || page == 'index.html') animIn='right-fade-in';
+    else if (page == navHrefs[2]) animIn='left-fade-in';
     
     $('#page-content').addClass(animIn);
     $('a.nav-item').click(function(event) {
         event.preventDefault();
         newLocation = this.href;
-        $('#page-content').addClass('down-fade-out');
         
-        setTimeout(function() {window.location = newLocation}, 250);
+        if (page == navHrefs[0] || page == 'index.html') animOut='left-fade-out';
+        else if (page == navHrefs[2]) animOut='right-fade-out';
+        
+        $('#page-content').addClass(animOut);
+        
+        setTimeout(function() {window.location = newLocation}, 500);
     });
 }
 

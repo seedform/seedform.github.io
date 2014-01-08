d51caa7b6662c2d6dbaf2b0cfe0012c56fb5515c
Shudmanul Chowdhury <seedformapps@gmail.com>
2014-01-08 02:25:21 -0500

    Gestures should now be working

--- js/pagef.js
+++ js/pagef.js
@@ -30,15 +30,19 @@ var SOC_TITLES = [
 
 var PAGE_EXIT_DELAY = 300;
 
-var page = location.pathname.substring(location.pathname.lastIndexOf('/') + 1);
-var prev = document.referrer.substring(document.referrer.lastIndexOf('/') + 1);
+var page = PAGES.indexOf(location.pathname.substring(location.pathname.lastIndexOf('/') + 1));
+var prev = PAGES.indexOf(document.referrer.substring(document.referrer.lastIndexOf('/') + 1));
+
+function goToDelayed(newLoc) {
+    setTimeout(function() { window.location = newLoc }, PAGE_EXIT_DELAY);
+}
 
 $(document).ready(function() {
 
     // Write the nav-bar to the page
     var nav = '<div class="vac">';
     for (var i = 0; i < PAGES.length; i++) {
-        nav += '<a class="nav-item ' + (page == PAGES[i] ? 'active' : '') + '" href="' + PAGES[i] + '">' + NAV_TITLES[i] + '</a>';
+        nav += '<a class="nav-item ' + (page == i ? 'active' : '') + '" href="' + PAGES[i] + '">' + NAV_TITLES[i] + '</a>';
         nav += i < PAGES.length - 1 ? '&nbsp;|&nbsp;' : '';
     }
     $("#nav").html(nav + '</div>');
@@ -64,19 +68,20 @@ $(document).ready(function() {
     $(window).resize(function() {
         fixFoot();
     });
+    fixFoot();
     
     // Set transition animations
     var animIn, animOut;
-    if (PAGES.indexOf(page) < PAGES.indexOf(prev)) animIn='right-fade-in';
-    else if (PAGES.indexOf(page) > PAGES.indexOf(prev)) animIn='left-fade-in';
+    if (page < prev) animIn='right-fade-in';
+    else if (page > prev) animIn='left-fade-in';
     $('#page-content').addClass(animIn);
     $('a.nav-item').click(function(event) {
         event.preventDefault();
         newLoc = this.getAttribute('href');        
-        if (PAGES.indexOf(page) < PAGES.indexOf(newLoc)) animOut='left-fade-out';
-        else if (PAGES.indexOf(page) > PAGES.indexOf(newLoc)) animOut='right-fade-out';
+        if (page < PAGES.indexOf(newLoc)) animOut='left-fade-out';
+        else if (page > PAGES.indexOf(newLoc)) animOut='right-fade-out';
         $('#page-content').addClass(animOut);
-        setTimeout(function() { window.location = newLoc }, PAGE_EXIT_DELAY);
+        goToDelayed(newLoc);
     });
 
     // Set all non-navigation links to open in a new tab
@@ -84,19 +89,19 @@ $(document).ready(function() {
         $('a').attr('target','_blank');
     });
 
-        // Touch gestures
-    var element = document.getElementById('page-content');
-    Hammer(element).on("swipeleft", function() {
-        $('#page-content').removeClass('left-fade-in right-fade-in');
-        $('#page-content').addClass('left-fade-out');
-        if (page + 1 <= PAGES.length) setTimeout(function() { window.location = PAGES[page + 1] }, PAGE_EXIT_DELAY);
-        else $('#page-content').addClass('right-fade-in');
+    // Touch gestures
+    $('#page-content').swipe({
+        swipe:function(event, direction, distance, duration, fingerCount) {
+            $('#page-content').removeClass('left-fade-in right-fade-in');
+            if (direction == 'left' && page + 1 < PAGES.length) {
+                $('#page-content').addClass('left-fade-out');
+                goToDelayed(PAGES[page + 1]);
+            } else if (direction == 'right' && page - 1 >= 0) {
+                $('#page-content').addClass('right-fade-out');
+                goToDelayed(PAGES[page - 1]);
+            }
+            
+        }
     });
-    Hammer(element).on("swiperight", function() {
-        $('#page-content').removeClass('left-fade-in right-fade-in');
-        $('#page-content').addClass('right-fade-out');
-        if (page - 1 >= 0) setTimeout(function() { window.location = PAGES[page - 1] }, PAGE_EXIT_DELAY);
-        else $('#page-content').addClass('left-fade-in');
-    });
-    
+
 });

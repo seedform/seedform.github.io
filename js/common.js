955e14bcf766dc566981295cdadcbe17d13efe88
Shudmanul Chowdhury <seedformapps@gmail.com>
2014-01-10 02:17:50 -0500

    Android theme!

--- js/common.js
+++ js/common.js
@@ -3,7 +3,7 @@ document.write(
     <link rel="stylesheet" href="css/styles.css" type="text/css" charset="utf-8"/> \
     <link rel="stylesheet" href="css/animations.css" type="text/css" charset="utf-8"/> \
     <link href="http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,300italic,700" rel="stylesheet" type="text/css"> \
-    <link href="http://fonts.googleapis.com/css?family=Roboto:400,700,400italic" rel="stylesheet" type="text/css"> \
+    <link href="http://fonts.googleapis.com/css?family=Roboto:400,300,300italic,700" rel="stylesheet" type="text/css"> \
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script> \
     <script src="js/jquery.touchSwipe.min.js"></script> \
     <script src="js/pagef.js"></script> \
